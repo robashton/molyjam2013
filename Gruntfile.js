356e403929b83bc114048dafcc2d5cf15d54d09e
Rob Ashton <robashton@codeofrob.com>
2013-07-05 19:36:59 +0100

    Skeleton

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      'public/app.js': ['/client/app.js'],
+      options: {
+        debug: true
+      }
+    },
+    watch: {
+      scripts: {
+        files: [ "/client/**/*.js"],
+        tasks: [ 'browserify' ]
+      }
+    },
+    concurrent: {
+      target: {
+        tasks: ['watch:scripts', 'nodemon:app'],
+        options: {
+          logConcurrentOutput: true
+        }
+      }
+    },
+    nodemon: {
+      app: {
+        options: {
+          file: "app.js",
+          ignoredFiles: ['/client/**/*.*', '/public/**/*.js']
+        }
+      }
+    }
+  })
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-concurrent')
+
+  grunt.registerTask('develop', ['browserify', 'concurrent'])
+}
---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express')
+  , http = require('http')
+  , path = require('path')
+
+var app = express()
+
+app.set('port', process.env.PORT || 6000)
+app.set('views', __dirname + '/server')
+app.set('view engine', 'jade')
+app.use(require('stylus').middleware(
+{src: __dirname + '/server', dest: __dirname + '/public'}))
+
+app.use(express.favicon())
+app.use(express.logger('dev'))
+app.use(express.bodyParser())
+app.use(express.methodOverride())
+app.use(app.router)
+app.use(express.static(path.join(__dirname, 'public')))
+
+if ('development' === app.get('env')) {
+  app.use(express.errorHandler())
+}
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'))
+})
